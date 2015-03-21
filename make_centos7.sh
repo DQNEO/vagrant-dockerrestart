dad611f12674ab5ab32aec09efe468bce0c72711
DQNEO <dqneoo@gmail.com>
2015-03-22 00:00:12 +0900

    automate vagrant box creation

---
+++ make_centos7.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# create my original vagrant box
+#
+# thanks to http://qiita.com/t_cyrill/items/ef9cb2b615bfb326f79c
+set -e
+
+vagrant up
+vagrant halt
+vagrant package
+vagrant box add mycentos7 package.box
+rm package.box
